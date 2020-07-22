8eb86843fea07afbc2dbee405d9ef1eb24abab0d
Ramin Roufeh <ramin1408@gmail.com>
2020-07-22 12:07:09 -0700

    update AWS API

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,12 @@
+const { AWSAccessKeyID } = require("./dev");
+
 module.exports ={
     MongoDB : process.env.MongoDB,
     FacebookAppID: process.env.FacebookID,
     FacebookAppSecret: process.env.FacebookSecret,
     GoogleClientID: process.env.GoogleID,
-    GoogleClientSecret: process.env.GoogleSecret
+    GoogleClientSecret: process.env.GoogleSecret,
+    AWSAccessKeyID: process.env.AWS_ACCESS_KEY,
+    AWSSecretAccessKey: process.env.AWS_ACCESS_SECRET
+
 };
