7e98f732d03754741e854e679ef5fb9f66ab2b35
Ramin Roufeh <ramin1408@gmail.com>
2020-07-22 07:14:59 -0700

    Google API update

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,7 @@
 module.exports ={
     MongoDB : process.env.MongoDB,
-    FacebookAppID: process.env.FaceBookID,
-    FacebookAppSecret: process.env.FaceBookSecret,
-    GoogleClientID: '252891987972-5mdbrf2di3u5ulqoq8dsnaio7vmomvbv.apps.googleusercontent.com',
-    GoogleClientSecret: 'QQMA05dG7-VhLIg1KanzXY3P'
-
+    FacebookAppID: process.env.FacebookID,
+    FacebookAppSecret: process.env.FacebookSecret,
+    GoogleClientID: process.env.GoogleID,
+    GoogleClientSecret: process.env.GoogleSecret
 };
