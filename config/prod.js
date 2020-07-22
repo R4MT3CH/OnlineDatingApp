86f2f0825f8dbd4920566abe949b2ebea05fe5ed
Ramin Roufeh <ramin1408@gmail.com>
2020-07-21 17:50:32 -0700

    mongoDB Update

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports ={
-    MongoDB : ' mongodb+srv://OnlineDatingApp:b7y6r6o9d@onlinedatingapp.5j592.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    MongoDB : process.env.MongoDB,
     FacebookAppID: '288763182368303',
     FacebookAppSecret: '553aa36baf2ff585fb7fcf7f5f15741d',
     GoogleClientID: '252891987972-5mdbrf2di3u5ulqoq8dsnaio7vmomvbv.apps.googleusercontent.com',
